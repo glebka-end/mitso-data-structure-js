671312822952e6eb9a6143effb16840899290661
glebka-end <77512536+glebka-end@users.noreply.github.com>
2021-12-18 12:06:57 +0300

    queue.js

--- src/queue.js
+++ src/queue.js
@@ -1,4 +1,5 @@
 const { NotImplementedError } = require('../extensions/index.js');
+const {ListNode} = require("../extensions");
 
 // const { ListNode } = require('../extensions/list-node.js');
 
@@ -14,20 +15,39 @@ const { NotImplementedError } = require('../extensions/index.js');
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
 module.exports = class Queue {
+  head = null;
+  tail = null;
 
   getUnderlyingList() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.head;
   }
 
-  enqueue(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  enqueue(value) {
+    const newNode = new ListNode(value);
+    if(!this.head || !this.tail){
+      this.head = newNode;
+      this.tail = newNode;
+      return this;
+    }
+
+    this.tail.next = newNode;
+    this.tail = newNode;
+    return this;
   }
 
   dequeue() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if(!this.head){
+      return null;
+    }
+    const deleteHead = this.head;
+    if (this.head.next) {
+      this.head = this.head.next;
+    } else {
+      this.head = null;
+      this.tail = null;
+    }
+
+    return deleteHead.value;
   }
 
 }
